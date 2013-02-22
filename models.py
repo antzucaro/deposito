4ba2133bde037cf49e9385387f13ad2a9164eab4
Ant Zucaro <azucaro@gmail.com>
2013-02-21 22:42:28 -0500

    Add users model, reorder others.

--- models.py
+++ models.py
@@ -6,30 +6,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 # and for more stuff (like a session), you'll need app.test_request_context()
 db = SQLAlchemy()
 
-class Map(db.Model):
-    map_id       = db.Column(db.Integer, primary_key=True)
-    name         = db.Column(db.String(100))
-    approved     = db.Column(db.Boolean)
-    downloadable = db.Column(db.Boolean)
-    create_by    = db.Column(db.Integer)
-    create_dt    = db.Column(db.DateTime)
-    update_by    = db.Column(db.Integer)
-    update_dt    = db.Column(db.DateTime)
-
-    def __init__(self, name, create_by):
-        self.name         = name
-        self.approved     = False
-        self.downloadable = False
-        self.create_by    = create_by
-        self.update_by    = create_by
-        self.create_dt    = datetime.utcnow()
-        self.update_dt    = self.create_dt
-
-    def __repr__(self):
-        return '<Map %s>' % self.name
-
 class File(db.Model):
-    file_id      = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'files'
+    __table_args__ = {'sqlite_autoincrement':True}
+
+    file_id      = db.Column(db.Integer, primary_key=True, autoincrement=True)
     filetype     = db.Column(db.String(100))
     filename     = db.Column(db.String(100))
     descr        = db.Column(db.String(300))
@@ -50,3 +31,55 @@ class File(db.Model):
 
     def __repr__(self):
         return '<File %s (%s)>' % (self.name, self.filetype)
+
+class Map(db.Model):
+    __tablename__ = 'maps'
+    __table_args__ = {'sqlite_autoincrement':True}
+
+    map_id       = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name         = db.Column(db.String(100))
+    approved     = db.Column(db.Boolean)
+    downloadable = db.Column(db.Boolean)
+    file_id      = db.Column(db.Integer)
+    create_by    = db.Column(db.Integer)
+    create_dt    = db.Column(db.DateTime)
+    update_by    = db.Column(db.Integer)
+    update_dt    = db.Column(db.DateTime)
+
+    def __init__(self, name, create_by):
+        self.name         = name
+        self.approved     = False
+        self.downloadable = False
+        self.create_by    = create_by
+        self.update_by    = create_by
+        self.create_dt    = datetime.utcnow()
+        self.update_dt    = self.create_dt
+
+    def __repr__(self):
+        return '<Map %s>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    __table_args__ = {'sqlite_autoincrement':True}
+
+    user_id     = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username    = db.Column(db.String(length=30), default='', unique=True)
+    password    = db.Column(db.String(length=100), default='')
+    active      = db.Column(db.Boolean, default=True)
+
+    is_authenticated = False
+
+    def is_authenticated(self):
+        return self.is_authenticated
+
+    def is_active(self):
+        return self.active
+
+    def is_anonymous(self):
+        return self.username != ""
+
+    def get_id(self):
+        return unicode(self.user_id)
+
+    def __repr__(self):
+        return '<User %s>' % self.username
