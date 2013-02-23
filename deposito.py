f70a53cf569bdd4c2babd56f5404d5981a7f7858
Ant Zucaro <azucaro@gmail.com>
2013-02-22 21:51:25 -0500

    Add map submission template.

--- deposito.py
+++ deposito.py
@@ -14,5 +14,9 @@ def main_index():
 
     return render_template('main_index.jinja', maps=maps)
 
+@app.route("/submit")
+def submit_map():
+    return render_template('submit_map.jinja')
+
 if __name__ == "__main__":
     app.run(debug=True)
