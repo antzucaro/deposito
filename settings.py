08a609efeeeac2059affa176282871a725c7b663
Ant Zucaro <azucaro@gmail.com>
2013-02-23 23:58:35 -0500

    Add SECRET_KEY config, alphabetize.

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
+SECRET_KEY = "This should be something really secret. Right now it isn't!"
+SQLALCHEMY_DATABASE_URI = 'sqlite:///deposito.db'
+SQLALCHEMY_ECHO = True
 UPLOAD_FOLDER = 'uploads'
 VALID_MAP_EXTENSIONS = set(['pk3', 'zip'])
 VALID_SS_EXTENSIONS  = set(['jpg', 'jpeg', 'png'])
-SQLALCHEMY_DATABASE_URI = 'sqlite:///deposito.db'
-SQLALCHEMY_ECHO = True
