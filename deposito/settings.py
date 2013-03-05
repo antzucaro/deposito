fccd4b6cc333915a2c9b1e8cacc104b5a89077fc
Ant Zucaro <azucaro@gmail.com>
2013-03-04 21:47:30 -0500

    For development, serve uploads.

--- deposito/settings.py
+++ deposito/settings.py
@@ -1,6 +1,6 @@
 SECRET_KEY = "This should be something really secret. Right now it isn't!"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///deposito.db'
 SQLALCHEMY_ECHO = True
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'deposito/static/uploads'
 VALID_MAP_EXTENSIONS = set(['pk3', 'zip'])
 VALID_SS_EXTENSIONS  = set(['jpg', 'jpeg', 'png'])
